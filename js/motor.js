45f734d1cd4a1133c1dccf9493c7b6228fe7110e
Mendler1994 <fernando.mendler@gmail.com>
2022-11-08 15:49:52 -0300

    funcionalidad

--- js/motor.js
+++ js/motor.js
@@ -1,1 +1,29 @@
-console.log(123);
+var estadoLicuadora = "apagado";
+var licuadora = document.getElementById("blender");
+var sonidoLicuadora = document.getElementById("blender-button-sound");
+var botonLicuadora = document.getElementById("blender-sound");
+
+function controlarLicuadora() {
+    if (estadoLicuadora == "apagado") {
+        estadoLicuadora = "encendido";
+        hacerRuido();
+        licuadora.classList.add("active");
+        console.log("Usted siempre me... enciende");
+    } else {
+        estadoLicuadora = "apagado"
+        hacerRuido();
+        licuadora.classList.remove("active");
+        console.log("me apagaste")
+    }
+}
+
+function hacerRuido(){
+    if (sonidoLicuadora.paused) {
+        botonLicuadora.play();
+        sonidoLicuadora.pause();
+    } else {
+        botonLicuadora.play();
+        sonidoLicuadora.play();
+        sonidoLicuadora.currentTime = 0;
+    }
+}
